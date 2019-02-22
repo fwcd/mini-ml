aaa5f03928ad69c26df934f26c3fd91a15de2962
fwcd <fwcdmail@gmail.com>
2019-02-22 14:54:26 +0100

    Added dot product gradient

--- src/main/kotlin/fwcd/miniml/graph/DotProduct.kt
+++ src/main/kotlin/fwcd/miniml/graph/DotProduct.kt
@@ -2,6 +2,7 @@ package fwcd.miniml.graph
 
 import fwcd.miniml.math.NDArray
 import fwcd.miniml.math.scalarOf
+import fwcd.miniml.math.ShapeMismatchException
 
 /**
  * The result of a dot product.
@@ -15,6 +16,14 @@ class DotProduct(
 	override fun forward() = scalarOf(operands[0].forward().dot(operands[1].forward()))
 	
 	override fun backward(gradient: NDArray) {
-		TODO("Dot product backprop not implemented")
+		val left = operands[0].cachedForward() ?: throw MissingCachedInputArray("Dot product")
+		val right = operands[1].cachedForward() ?: throw MissingCachedInputArray("Dot product")
+		
+		if (!left.shape.contentEquals(gradient.shape)) {
+			throw ShapeMismatchException("Gradient of dot product", left.shape, gradient.shape)
+		}
+		
+		operands[0].backward(right * gradient)
+		operands[1].backward(left * gradient)
 	}
 }
